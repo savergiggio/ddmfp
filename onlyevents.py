cb4ae3451cf0bed7dd1bcaa9624890d7d97877c9
savergiggio <44522558+savergiggio@users.noreply.github.com>
2025-05-23 16:50:15 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -9,8 +9,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://mam.lokiale.duckdns.org"     # non mettere lo / finale al link
+MFPPSW = "Digimon96.."
 
 # Costanti
 NUM_CHANNELS = 10000
